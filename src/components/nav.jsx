9adf7fc0094599a60184ca609061890de4f9604c
Sparsh Gupta <spartan090907@gmail.com>
2024-02-28 21:00:25 +0000

    s

---
+++ src/components/nav.jsx
@@ -0,0 +1,38 @@
+import Logo from '../assets/logo.svg';
+import { A } from "@solidjs/router";
+import { IoOptions } from 'solid-icons/io';
+import { IoGameController } from 'solid-icons/io';
+import { SiDiscord } from 'solid-icons/si';
+import { AiOutlineGithub } from 'solid-icons/ai';
+
+function Nav() {
+  function gamesGo() {
+    window.navigate("https://radon.games/games")
+  }
+
+  return (
+    <>
+      <div class="nav">
+        <A href="/" class="logo">
+            <Logo />
+        </A>
+        <div class="navItems">
+            <a href="https://google.com" class="navItem">
+                <SiDiscord fill="var(--text-inverse)" />
+            </a>
+            <a href="https://google.com" class="navItem">
+                <AiOutlineGithub fill="var(--text-inverse)" />
+            </a>
+            <div onclick={gamesGo} class="navItem">
+                <IoGameController fill="var(--text-inverse)" />
+            </div>
+            <A href="/options" class="navItem">
+                <IoOptions fill="var(--text-inverse)" />
+            </A>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Nav;
